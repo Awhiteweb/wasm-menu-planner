004d38d0b89eb9ed298d9228d9b5e589ba3e5be8
Awhiteweb <alex_p_white@yahoo.co.uk>
2021-02-07 15:58:11 +0000

    Updating lib adding url and trying to fix errros

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //  - it's useful when you want to check your code with `cargo make verify`
 // but some rules are too "annoying" or are not applicable for your case.)
 #![allow(clippy::wildcard_imports)]
-#![allow(unused_imports)]
+#![allow(dead_code, unused_imports)]
 
 use seed::{prelude::*, *};
 
@@ -16,9 +16,10 @@ use chrono::naive::NaiveDate;
 // ------ ------
 
 // `init` describes what should happen when your app started.
-fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+fn init(url: Url, _: &mut impl Orders<Msg>) -> Model {
     Model {
-        current_week_date: NaiveDate::from_ymd(2021, 02, 01)
+        current_week_date: NaiveDate::from_ymd(2021, 02, 01),
+        base_url: url.to_base_url()
     }
 }
 
@@ -28,7 +29,8 @@ fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
 
 // `Model` describes our app state.
 struct Model {
-    current_week_date: NaiveDate
+    current_week_date: NaiveDate,
+    base_url: Url
 }
 
 // example seed-rs model for using routing and auth
@@ -65,7 +67,7 @@ struct Model {
 // #[derive(Copy, Clone)]
 // `Msg` describes the different events you can modify state with.
 enum Msg {
-    // UrlChanged(subs::UrlChanged)
+    UrlChanged(subs::UrlChanged),
     NextWeekTransition,
     PreviousWeekTransition,
 }
@@ -73,6 +75,7 @@ enum Msg {
 // `update` describes how to handle each `Msg`.
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
+        Msg::UrlChanged(subs::UrlChanged(url)) => {},
         Msg::NextWeekTransition => {
             model.current_week_date = model.current_week_date + Duration::days(7)
         },
@@ -95,12 +98,12 @@ fn view(model: &Model) -> Node<Msg> {
         div![
             "Previous week <<",
             C!["counter"],
-            button![model, ev(Ev::Click, |_| Msg::PreviousWeekTransition),],
+            button![ev(Ev::Click, |_| Msg::PreviousWeekTransition),],
         ],
         div![
             "Next week >>",
             C!["counter"],
-            button![model, ev(Ev::Click, |_| Msg::NextWeekTransition),],
+            button![ev(Ev::Click, |_| Msg::NextWeekTransition),],
         ]
     ]
 }
